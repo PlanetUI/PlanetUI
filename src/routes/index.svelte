c11d0f9299c1729b1fabcb16feb64955a474b1f3
mashanz <mangatkk@gmail.com>
2022-06-21 23:27:30 +0700

    [ADD] Table library

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
     import Input from '$lib/Input.svelte';
+    import Table from '$lib/Table.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Input type="password" />
+<!-- <Input type="password" /> -->
+
+<Table heads={['Name', 'Age']} rows={[
+    ['Are John', '25'],
+    ['Laku Kane', '26'],
+    ['Mane Joe', '27'],
+    ['Bae Dack', '28'],
+    ['Lao Kill', '29'],
+
+    ['Kas Staven', '40'],
+    ['Tare Lloyd', '12'],
+    ['Gurd John', '25'],
+    ['Pan Kane', '26'],
+    ['Fol Joe', '27'],
+    
+    ['Bar Dack', '28'],
+    ['Olim Kill', '29'],
+    ['Xo Staven', '40'],
+    ['Dan Lloyd', '12'],
+    ['John Doe', '25'],
+    
+    ['Kane Jo', '26'],
+    ['Joe Xi', '27'],
+    ['Dack Pa', '28'],
+    ['Kill Bee', '29'],
+    ['Staven Yu', '40'],
+    
+    ['Lloyd Mark', '12'],
+]} />
