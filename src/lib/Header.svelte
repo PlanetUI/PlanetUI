814c313463440adb4b9ccd1695a2458e37f3a89a
mashanz <mangatkk@gmail.com>
2022-06-30 20:09:30 +0700

    [ADD] style Head

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<header class="flex p-5 bg-gray-200">
+	<h1 class="flex-grow font-bold">
+		<a sveltekit:prefetch href="/" class="logo"> PlanetUI </a>
+	</h1>
+	<nav class="flex">
+		<ul class="flex gap-5">
+			<li>
+				<a sveltekit:prefetch href="/">Home</a>
+			</li>
+			<li>
+				<a sveltekit:prefetch href="/about">About</a>
+			</li>
+			<li>
+				<a sveltekit:prefetch href="/contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '$lib/app.css';
+	import Header from '$lib/Header.svelte';
 </script>
+
+<Header />
 
 <slot />
